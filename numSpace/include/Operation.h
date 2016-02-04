097417fb4afe28f8c31079869e7d704564256bb5
photobuff99 <photobuff99@users.noreply.github.com>
2016-02-04 12:33:02 -0700

    Update Operation.h

--- numSpace/include/Operation.h
+++ numSpace/include/Operation.h
@@ -10,9 +10,15 @@ class Operation
     public:
         Operation();
         virtual ~Operation();
+        // Getters 
+        int getSubSize(); // wiil get the size of the 
+        // Setters / Adders
         void setOpID(int opID);
         int setupOp(Operation* parent);
         int addChild(Operation* child);
+        int setVal(Number* _val); // will set the number value of a null operation number
+        // Validators
+        int isOpValid(); // will check if any of the ops children are its parents
     protected:
         Operation* upOp; // Operation above current op
         std::vector<Operation*> subOp; // Operations below the current op
