79709df5800028505ca051dc9f0b57ba7595d0e9
photobuff99 <photobuff99@users.noreply.github.com>
2016-02-04 12:07:31 -0700

    Update Expression.h

--- numSpace/include/Expression.h
+++ numSpace/include/Expression.h
@@ -14,8 +14,8 @@ class Expression
         virtual ~Expression();
         //void printExpression();
         int pushNumber(Number*);
-        Operation* addOp(int OpID);
-        int makeSubOp(Operation* parent, Operation* child);
+        Operation* addOp(int OpID); // Creates a new Operation and adds it to the opTree vector, takes the OpID of the new operation
+        int makeSubOp(Operation* parent, Operation* child); // given op a and b in opTree, makes b a sub op of b
     protected:
     private:
        std::vector<Number*> numList;
